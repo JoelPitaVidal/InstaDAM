f229045e579e372e6f0d3c2053190fdc25d34bae
Joel Pita Vidal <jpitavidal@danielcastelao.org>
2024-04-15 11:16:51 +0200

    ultimos comentarios

--- src/menuInstaDAM.java
+++ src/menuInstaDAM.java
@@ -1,7 +1,14 @@
+//interfaz con los metodos que se implementan en la clase InstaDAM
+
 public interface menuInstaDAM {
+//Metodo para registrarse
     void registrarse();
+//Metodo para hacer una nueva publicación
     void hacerPublicacion();
+//Metodo para iniciar la sesión de un nuevo usuario
     void iniciarSesion();
+//Metodo para ver las publicaciones realizadas por un usuario
     void verNumPublicacion();
+//Metodo para ver el contenido de las publicaciones
     void verContenidoPublicaciones();
 }
